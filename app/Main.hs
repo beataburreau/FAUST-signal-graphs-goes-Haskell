9a39f15dd6026a3308c888c689a568dc921088f0
Beata Burreau <beata@speedment.com>
2022-05-02 16:06:08 +0200

    User-friendlier main

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,13 @@
 module Main where
 
 import SignalGraph (parseNprintGraph)
+import Data.List (isSuffixOf)
 
 main :: IO ()
-main = getLine >>= parseNprintGraph
+main = do
+        putStrLn "Enter relative file path to a .dot file:"
+        path <- getLine
+        if ".dot" `isSuffixOf` path
+        then parseNprintGraph path
+        else errorWithoutStackTrace "The provided file is not a .dot file"
 
