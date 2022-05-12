ea26c04920e3221011b08c167d7e5c06ac603add
Beata Burreau <beata@speedment.com>
2022-05-12 18:49:56 +0200

    Detailed graph print using custom printer function prettyDetailedPrint

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
 import SignalGraph (haskelliseFile)
 import Data.List (isSuffixOf)
-import GraphMonad (runGraphM, initState)
-import Data.Graph.Inductive (prettyPrint)
+import GraphMonad (runGraphM, initState, State)
+import GraphPrinter (prettyDetailedPrint)
 
 -- Parses the content of given file into an inductive dynamic graph and prints it
 main :: IO ()
@@ -11,9 +12,9 @@ main = do
         putStrLn "Enter relative file path to a .dot file:"
         path <- getLine
         if ".dot" `isSuffixOf` path
-        then do 
+        then do
                 res <- runGraphM initState (haskelliseFile path)
                 case res of
                         Left err         -> putStrLn ("\nParse failed with error:\n" ++ show err)
-                        Right (graph, _) -> prettyPrint graph >> putStrLn "Parse successful!"
+                        Right (_, state) -> prettyDetailedPrint state >> putStrLn "Parse successful!"
         else errorWithoutStackTrace "The provided file is not a .dot file"
--- src/GraphPrinter.hs
+++ src/GraphPrinter.hs
@@ -1,17 +1,57 @@
-module GraphPrinter (prettyPrint) where 
+{-# LANGUAGE LambdaCase #-}
+module GraphPrinter (prettyPrint, prettyDetailedPrint) where
 
 -- LIBRARIES
-import Data.Graph.Inductive (DynGraph, context, nodes)
+import Data.Graph.Inductive (DynGraph, context, nodes, Context)
+import GraphMonad (State, Node(Node), Edge(Edge))
+import Data.Interval (Interval, Extended (..), lowerBound, upperBound)
+
+
+-- Pretty-print the graph details to stdout.
+prettyDetailedPrint :: State -> IO ()
+prettyDetailedPrint = putStr . prettifyDetailed
+
+-- Pretty-print the graph details (kept in the state)
+prettifyDetailed :: State -> String
+prettifyDetailed (ns, es) = foldr showsNode id ns "" ++ foldr showsEdge id es ""
+        where
+                showsNode :: Node a -> (a2 -> String) -> a2 -> String
+                showsNode (Node i id t p r) sg = shows i 
+                                        . showString ": " . shows p
+                                        . showString ", " . shows r
+                                        . showString ", " . shows t
+                                        . showString "\n\n" . sg
+                showsEdge :: Edge a -> (a2 -> String) -> a2 -> String
+                showsEdge (Edge i i1 i2 t a interval) sg = shows i1 . showString " -> " . shows i2 
+                                                . showString ": Argument " . shows a
+                                                . showString (", " ++ prettifyInterval interval)
+                                                . showString ", " . shows t
+                                                . showString "\n\n" . sg        
 
 -- Pretty-print the graph to stdout.
 prettyPrint :: (DynGraph gr, Show a, Show b) => gr a b -> IO ()
 prettyPrint = putStr . prettify
 
--- | Pretty-print the graph.  Note that this loses a lot of
---   information, such as edge inverses, etc.
+-- Pretty-print the graph.  Note that this loses a lot of
+-- information, such as edge inverses, etc.
 prettify :: (DynGraph gr, Show a, Show b) => gr a b -> String
 prettify g = foldr (showsContext . context g) id (nodes g) ""
   where
-    showsContext (_,n,l,s) sg = shows n . (':':) . shows l
+    showsContext :: (Show a, Show a1) => Context a a1 -> (a2 -> String) -> a2 -> String
+    showsContext (_,n,l,s) sg = shows n . showString ": " . shows l
                                 . showString " -> " . shows s
                                 . showString "\n\n" . sg
+
+
+prettifyInterval :: Maybe (Interval Float) -> String
+prettifyInterval Nothing   = "[?, ?]"
+prettifyInterval (Just i)  = lower ++ ", " ++ upper
+                    where
+                        lower = case lowerBound i of
+                                PosInf      -> "(inf"
+                                NegInf      -> "(-inf"
+                                (Finite f)  -> "[" ++ show f
+                        upper = case upperBound i of
+                                PosInf      -> "inf)"
+                                NegInf      -> "-inf)"
+                                (Finite f)  -> show f ++ "]"
